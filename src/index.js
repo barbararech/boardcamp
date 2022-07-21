2ed7cd45afd704d054c54eb0b1a508dcf612ad10
barbararech <rech.brb@gmail.com>
2022-07-21 10:01:48 -0300

    feat: implement database connection

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log("Server On!"));
