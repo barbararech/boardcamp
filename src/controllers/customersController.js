94ea29d1a020c76ea8b295cf56606b0d43255345
barbararech <rech.brb@gmail.com>
2022-07-22 14:13:49 -0300

    feat: add endpoint get customer

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -21,3 +21,24 @@ export async function getCustomers(req, res) {
   }
 }
 
+export async function getCustomer(req, res) {
+    const { id } = req.params;
+  
+    try {
+      const { rows: customer } = await connection.query(
+        `SELECT * FROM customers WHERE id = ${id};`
+      );
+  
+      if (customer.length === 0) {
+        return res.sendStatus(404);
+      }
+  
+      return res.send(customer);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  }
+  
+  
+  
