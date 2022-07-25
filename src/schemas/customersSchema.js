49dd27731fd85c6dece411c1d80f0c502979ccdc
barbararech <rech.brb@gmail.com>
2022-07-25 10:20:30 -0300

    fix: fix customer birthday schema

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -1,9 +1,9 @@
 import joi from "joi";
-import dayjs from "dayjs";
+
 
 export const customerSchema = joi.object({
   name: joi.string().required(),
   cpf: joi.string().length(11).required(),
   phone: joi.string().min(10).max(11).required(),
-  birthday: joi.date().iso().required().min("1900-01-01").max(Date.now()),
+  birthday: joi.date().required().min("1900-01-01").max(Date.now()),
 });
